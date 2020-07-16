2465c9df90406b02246903d40eba8169acf9ce84
[Evgeniy] <[tron1100@mail.ru]>
2020-07-16 15:57:48 +0300

    blabla

---
+++ pool.py
@@ -0,0 +1,18 @@
+# put your python code here
+from math import ceil
+a = 6  # float(input()) #storona m
+b = 150  # float(input()) #rashod ml
+v = 3  # int(input()) #banka ob
+
+if a <= 1 or b <= 50 or v <= 0:
+    print("error")
+    exit()
+
+S = 5*pow(a, 2)
+B_1 = b/1000
+B = B_1*S
+Q = B/v
+print(ceil(Q), S, B)
+print(round(Q))
+nghfvhgf
+kljlkj
