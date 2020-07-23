4c78ce9a1885edc4ca609ea00ad51790cff39407
[Evgeniy] <[tron1100@mail.ru]>
2020-07-23 10:39:25 +0300

    update this is file

---
+++ compute_function.py
@@ -0,0 +1,16 @@
+from math import inf
+
+
+def f_x(x):
+    try:
+        y = 1 / (x + 1) + x / (x - 3)
+    except:
+        y = inf
+    return y
+
+
+t = float(input("t = "))
+
+y = f_x(t)
+
+print("f(%4.2f) = %6.3f" % (t, y))
